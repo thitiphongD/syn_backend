e61a4471b06f289613b8dc2e9ecbd278f1e10884
Thitiphong Srisavat <80618380+thitiphongD@users.noreply.github.com>
2023-12-14 15:32:07 +0700

    add vercel

--- app.js
+++ app.js
@@ -26,6 +26,8 @@ app.get('*', (req, res) => {
 })
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}`)
-})
+
+module.exports = app;
+// app.listen(port, () => {
+//     console.log(`Server listening on port ${port}`)
+// })
